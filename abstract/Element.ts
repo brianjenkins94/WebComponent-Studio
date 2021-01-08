49997c7fa81ea9e08a8413102cad8f9dd5669a2f
Brian Jenkins <brianjenkins94@gmail.com>
2021-01-07 22:15:50 -0500

    Added transparent content, not sure if we're going to use it

--- abstract/Element.ts
+++ abstract/Element.ts
@@ -1,6 +1,7 @@
 import type { IEventEmitter } from "./EventEmitter";
 import type { ElementAttributesMap } from "../types/attributes";
 import type { TopLevelElementMap } from "../types/elements";
+import { FlowContent, InteractiveContent, PhrasingContent } from "../types/content";
 
 export abstract class Element<TagName extends keyof TopLevelElementMap> implements IEventEmitter {
 	protected template: HTMLElement;
@@ -55,11 +56,124 @@ export abstract class Element<TagName extends keyof TopLevelElementMap> implemen
 		});
 	}
 
+	// eslint-disable-next-line complexity
 	public push(...items: (string | Element<TagName>)[]): this {
 		for (const item of items) {
 			if (typeof item === "string") {
 				this.children.push(item);
 			} else {
+				switch (this.type) {
+					// Flow content
+
+					// excluding self
+					case "form":
+						if (this.type === item.type) {
+							throw new Error("<" + item.type + "> is not a permitted child of <" + this.type + ">.");
+						}
+
+					// excluding <header> and <footer>
+					case "footer":
+					case "header":
+						if ((this.type === "header" || this.type === "footer") && (item.type === "header" || item.type === "footer")) {
+							throw new Error("<" + item.type + "> is not a permitted child of <" + this.type + ">.");
+						}
+
+					// (excluding interactive content) or phrasing content
+					case "a":
+						if (this.type === "a" && (InteractiveContent.includes(item.type) || !PhrasingContent.includes(item.type))) {
+							throw new Error("<" + item.type + "> is not a permitted child of <" + this.type + ">.");
+						}
+
+					case "article":
+					case "aside":
+					case "blockquote":
+					case "details":
+					case "div":
+					case "fieldset":
+					case "figure":
+					case "main":
+					case "nav":
+					case "section":
+						if (!FlowContent.includes(item.type)) {
+							throw new Error("<" + item.type + "> is not a permitted child of <" + this.type + ">.");
+						}
+						break;
+
+					// Phrasing content
+
+					// excluding self
+					case "label":
+					case "meter":
+					case "progress":
+						if (this.type === item.type) {
+							throw new Error("<" + item.type + "> is not a permitted child of <" + this.type + ">.");
+						}
+
+					// excluding Interactive content
+					case "button":
+						if (this.type === "button" && InteractiveContent.includes(item.type)) {
+							throw new Error("<" + item.type + "> is not a permitted child of <" + this.type + ">.");
+						}
+
+					case "b":
+					case "code":
+					case "em":
+					case "h1":
+					case "h2":
+					case "h3":
+					case "h4":
+					case "h5":
+					case "h6":
+					case "i":
+					case "kbd":
+					case "mark":
+					case "p":
+					case "pre":
+					case "q":
+					case "s":
+					case "small":
+					case "span":
+					case "strong":
+					case "sub":
+					case "sup":
+					case "u":
+						if (!PhrasingContent.includes(item.type)) {
+							throw new Error("<" + item.type + "> is not a permitted child of <" + this.type + ">.");
+						}
+						break;
+
+					// No content
+					case "br":
+					case "hr":
+					case "iframe":
+					case "img":
+					case "input":
+						throw new Error("<" + item.type + "> is not a permitted child of <" + this.type + ">.");
+
+					// `<img>`-only
+					case "picture":
+						if (item.type !== "img") {
+							throw new Error("<" + item.type + "> is not a permitted child of <" + this.type + ">.");
+						}
+
+					// No <audio> or <video>
+					case "audio":
+					case "video":
+						if ((this.type === "audio" || this.type === "video") && (item.type === "audio" || item.type === "video")) {
+							throw new Error("<" + item.type + "> is not a permitted child of <" + this.type + ">.");
+						}
+
+					// No interactive content except for <a>, <button>, and <input> elements whose type attribute is checkbox, radio, or button
+					case "canvas":
+						if (!InteractiveContent.includes(item.type) && (item.type === "a" || item.type === "button" || item.type === "input")) {
+							throw new Error("<" + item.type + "> is not a permitted child of <" + this.type + ">.");
+						}
+
+					case "del":
+					case "ins":
+					default:
+				}
+
 				this.children.push(item.toString());
 			}
 		}
